eaff50a8b74a1da47c38891d4980aa9e22ae45c0
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2024-06-26 17:39:28 -0600

    shrink wasm binary

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use leptos::*;
 
+/*
+// Use `wee_alloc` as the global allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+*/
+
 fn main() {
     let world = "world";
     mount_to_body(move || view! { <p>"Hello, " {world} "!"</p> })
